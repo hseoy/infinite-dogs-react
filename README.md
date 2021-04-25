4d9e7926e88f20b0956849a6c774b9383022727e
Yunseo Hwang <yunseo.dev@gmail.com>
2021-04-25 14:18:10 +0900

    Initial commit
    Signed-off-by: Yunseo Hwang <yunseo.dev@gmail.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+# Infinite Dogs React
+
+Using the [Unsplash API](https://github.com/hseoy/unsplash-api), get dog photos and implement infinite scrolling. I reimplemented [Infinite Dogs](https://github.com/hseoy/infinite-dogs) using React.
---
+++ src/GlobalStyle.js
@@ -0,0 +1,63 @@
+import { createGlobalStyle } from 'styled-components';
+import 'assets/fonts/fonts.css';
+
+const GlobalStyle = createGlobalStyle`
+  :hover, :focus {
+    outline: none;
+  }
+
+  #root {
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    position: relative;
+  }
+
+  html, body {
+    width: 100%;
+    height: 100%;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    margin: 0;
+    font-family: "Bebas Neue", cursive, sans-serif;
+  }
+
+  body, h1, h2, h3, h4, h5, h6,
+  p, blockquote, pre, hr,
+  dl, dd, ol, ul, figure,
+  input, button, th, td, textarea, div {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    border: none;
+    outline: none;
+  }
+
+  ul, ol, li {
+    list-style: none;
+  }
+
+  button {
+    border: 0 none;
+    border-radius: 0;
+    background-color: transparent;
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+
+    :hover {
+      text-decoration: underline;
+    }
+  }
+
+  img, input, button {
+    vertical-align: middle;
+  }
+`;
+
+export default GlobalStyle;
